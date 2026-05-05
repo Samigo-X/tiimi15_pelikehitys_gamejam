97e39525a6b47ee8f333060836dfb8afe6c65be0
Daniel <daniel.sandberg@tuni.fi>
2026-05-05 23:42:52 +0300

    Earlier project wiped, found better solution

--- new-game-project/Scripts/main.gd
+++ new-game-project/Scripts/main.gd
@@ -1,40 +1,99 @@
-extends Control
+extends Node2D
 
-const GRID_WIDTH: int = 10
-const GRID_HEIGHT: int = 20
-const BLOCK_SIZE: int = 30
+var i_tetromino: Array = [
+	[Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(3, 1)], # 0 degrees
+	[Vector2i(2, 0), Vector2i(2, 1), Vector2i(2, 2), Vector2i(2, 3)], # 90 degrees
+	[Vector2i(0, 2), Vector2i(1, 2), Vector2i(2, 2), Vector2i(3, 2)], # 180 degrees
+	[Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2), Vector2i(1, 3)]  # 270 degrees
+]
+ 
+var t_tetromino: Array = [
+	[Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)], # 0 degrees
+	[Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)], # 90 degrees
+	[Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)], # 180 degrees
+	[Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2)]  # 270 degrees
+]
+ 
+var o_tetromino: Array = [
+	[Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)], # All rotations are the same
+	[Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)], # All rotations are the same
+	[Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)], # All rotations are the same
+	[Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)]  # All rotations are the same
+]
+ 
+var z_tetromino: Array = [
+	[Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1)], # 0 degrees
+	[Vector2i(2, 0), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)], # 90 degrees
+	[Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2), Vector2i(2, 2)], # 180 degrees
+	[Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(0, 2)]  # 270 degrees
+]
+ 
+var s_tetromino: Array = [
+	[Vector2i(1, 0), Vector2i(2, 0), Vector2i(0, 1), Vector2i(1, 1)], # 0 degrees
+	[Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1), Vector2i(2, 2)], # 90 degrees
+	[Vector2i(1, 1), Vector2i(2, 1), Vector2i(0, 2), Vector2i(1, 2)], # 180 degrees
+	[Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2)]  # 270 degrees
+]
+ 
+var l_tetromino: Array = [
+	[Vector2i(2, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)], # 0 degrees
+	[Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2), Vector2i(2, 2)], # 90 degrees
+	[Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(0, 2)], # 180 degrees
+	[Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2)]  # 270 degrees
+]
+ 
+var j_tetromino: Array = [
+	[Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)], # 0 degrees
+	[Vector2i(1, 0), Vector2i(2, 0), Vector2i(1, 1), Vector2i(1, 2)], # 90 degrees
+	[Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(2, 2)], # 180 degrees
+	[Vector2i(1, 0), Vector2i(1, 1), Vector2i(0, 2), Vector2i(1, 2)]  # 270 degrees
+]
 
-var grid: Array[Array] = []
-@onready var game_area: ColorRect = $GameArea
-@onready var grid_lines: Node2D = $GameArea/GridLines
+var tetrominoes: Array = [i_tetromino, t_tetromino, o_tetromino, z_tetromino, s_tetromino, l_tetromino, j_tetromino]
+var all_tetrominoes: Array = tetrominoes.duplicate()
+
+const START_POSITION: Vector2i = Vector2i(5, 1)
+var current_position : Vector2i
+
+var current_tetromino_type: Array
+var next_tetromino_type: Array
+var rotation_index: int = 0
+var active_tetromino: Array = []
+
+var tile_id: int = 0
+var piece_atlas: Vector2i
+var next_piece_atlas: Vector2i
+
+@onready var board_layer: TileMapLayer = $Board
+@onready var active_layer: TileMapLayer = $Active
 
 func _ready() -> void:
-	initialize_grid()
-	draw_grid_lines()
+	start_new_game()
 	
-func initialize_grid() -> void:
-	for x in range(GRID_WIDTH):
-		var column: Array[Variant] = []
-		for y in range(GRID_HEIGHT):
-			column.append(null)
-		grid.append(column)
+func start_new_game() -> void:
+	current_tetromino_type = choose_tetromino()
+	piece_atlas = Vector2i(all_tetrominoes.find(current_tetromino_type), 0)
+	initialize_tetromino()
+	
+	
+func choose_tetromino() -> Array:
+	var selected_tetromino: Array
+	if not tetrominoes.is_empty():
+		tetrominoes.shuffle()
+		selected_tetromino = tetrominoes.pop_front()
+	else:
+		tetrominoes = all_tetrominoes.duplicate()
+		tetrominoes.shuffle()
+		selected_tetromino = tetrominoes.pop_front()
+	return selected_tetromino
+
+func initialize_tetromino() -> void:
+	current_position = START_POSITION
+	active_tetromino = current_tetromino_type[rotation_index]
+	render_tetromino(active_tetromino, current_position, piece_atlas)
+
+func render_tetromino(tetromino: Array, grid_position: Vector2i, atlas: Vector2i) -> void:
+	for block in tetromino:
+		board_layer.set_cell(grid_position + block, tile_id, atlas)
 		
-func draw_grid_lines() -> void:
-	for x in range(GRID_WIDTH + 1):
-		var line: Line2D = Line2D.new()
-		line.add_point(Vector2(x * BLOCK_SIZE, 0))
-		line.add_point(Vector2(x * BLOCK_SIZE, GRID_HEIGHT * BLOCK_SIZE))
-		line.width = 1
-		line.default_color = Color.WHITE
-		grid_lines.add_child(line)
-		
-	for y in range(GRID_HEIGHT + 1):
-		var line: Line2D = Line2D.new()
-		line.add_point(Vector2(0, y * BLOCK_SIZE))
-		line.add_point(Vector2(GRID_WIDTH * BLOCK_SIZE, y * BLOCK_SIZE))
-		line.width = 1
-		line.default_color = Color.WHITE
-		grid_lines.add_child(line)
-		
-		
-		
+	
